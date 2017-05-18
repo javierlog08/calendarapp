a4f3180da2d5f90111fa8c1abc85eca8bc9c56b0
Javier Girón <javierlog08@gmail.com>
2017-05-18 18:24:09 -0500

    Added angular material to the project ->https://material.angularjs.org/latest/

--- app/app.js
+++ app/app.js
@@ -3,12 +3,17 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngMaterial',
   'myApp.view1',
   'myApp.view2',
+  'myApp.loginComponent',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function($locationProvider, $routeProvider, $mdThemingProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/login'});
+
+  /*$mdThemingProvider.theme('default')
+    .primaryPalette('blue'); */
 }]);
---
+++ app/login/login.component.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('myApp.loginComponent', ['ngRoute', 'ngMaterial'])
+.config(function($routeProvider) {
+  $routeProvider.when('/login', {
+    template: '<login-component></login-component>'
+  });
+})
+.component('loginComponent', {
+  controller: LoginComponent
+});
+
+function LoginComponent($mdDialog) {
+    
+    this.model = {
+        username: "",
+        password: ""
+    }
+
+    $mdDialog.show({
+      templateUrl: 'login/login.component.html',
+      clickOutsideToClose:false,
+      fullscreen: true,
+      parent:angular.element(document.body),
+    });
+
+}
