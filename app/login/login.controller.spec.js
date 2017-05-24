3a9f9c79054ce792a01a570b667b85cc06ba89c6
Javier Girón <javierlog08@gmail.com>
2017-05-23 21:52:12 -0500

    Finished unit testing over loginController

--- app/login/login.controller.spec.js
+++ app/login/login.controller.spec.js
@@ -1,26 +1,48 @@
-describe('loginController', function() {
+describe('Testing loginController', function() {
   
   var $loginController;
-  var $form;
+  var $formMock;
   var $rootScope;
-  var $scope;
+  var loaderServiceSpy;
+  var autServiceSpy;
+  var loginDialogSpy;
+  var locationSpy;
 
   beforeEach(function() {
     module('app.loginComponent');
     module('app.shared');
   });
-  beforeEach(inject(function($controller, authService, $q, _$rootScope_) {
+  beforeEach(inject(function($controller, authService, loaderService, $mdDialog, $location, _$rootScope_, $q) {
 
     $rootScope = _$rootScope_;
-    $scope = $rootScope.$new();
-    $loginController = $controller("loginController",{
-      scope:$scope
-    });
+    $loginController = $controller("loginController");
+    loaderServiceSpy = loaderService;
+    loginDialogSpy = $mdDialog;
+    locationSpy = $location;
+    authServiceSpy = authService;
+    
+    $formMock = {
+      username : {
+        $setValidity : function(){},
+      },
+      password: {
+        $setValidity : function(){},
+      },
+      $setSubmitted : function(){}
+    }
 
-    spyOn(authService,"login").and.callFake(function(model) { 
+    spyOn($formMock.username,"$setValidity");
+    spyOn($formMock.password,"$setValidity");
+    spyOn($formMock,"$setSubmitted");
+    spyOn(loaderServiceSpy,"showLoader");
+    spyOn(loaderServiceSpy,"hideLoader");
+    spyOn(loginDialogSpy,"hide");
+    spyOn(locationSpy,"url");
+
+    spyOn(authServiceSpy,"login").and.callFake(function(model) { 
        var defer = $q.defer();
        var errors =  [];
-       if(model.username == "myuser" && mode.password == "mypass") {
+       if(model.username == "myuser" && model.password == "mypass") {
           defer.resolve(errors);
        } else {
          errors.push({"login-error":"Wrong username or password"});
@@ -32,18 +54,81 @@ describe('loginController', function() {
 
   }));
 
-  it('loginController - should have a  member model for the form defined', function() {
+  it('>> loginController - should have a  member model for the form defined', function() {
     expect($loginController.model).toBeDefined();
   });
 
-  it('loginController - auth method should set loginForm invalid when calling with wrong user credentials.',function() {
-        
-    $loginController.auth($scope.loginForm);
+  it('>> loginController.auth() - if is auth called should show a loader with loaderService',function() {
+    $loginController.auth($formMock);
 
     $rootScope.$apply(); // resolve promises in the services
 
-    expect($loginController.loginForm.$invalid).toBeTruthy();
+    expect(loaderServiceSpy.showLoader).toHaveBeenCalled();
+  });
 
+  it('>> loginController.auth() - should call authService.login to authenticated',function() {
+    $loginController.auth($formMock);
+
+    $rootScope.$apply(); // resolve promises in the services
+
+    expect(authServiceSpy.login).toHaveBeenCalled();
+  });
+
+  it('>> loginController.auth() - should hide a loader once authService resolve login',function() {
+    $loginController.auth($formMock);
+
+    $rootScope.$apply(); // resolve promises in the services
+
+    expect(loaderServiceSpy.hideLoader).toHaveBeenCalled();
+  });
+
+  it('>> loginController.auth() - should set validation error on formController inputs',function() {
+
+    $loginController.model.username = "wronguser";
+    $loginController.model.password = "wrongpass";
+
+    $loginController.auth($formMock);
+
+    $rootScope.$apply(); // resolve promises in the services
+
+    expect($formMock.username.$setValidity).toHaveBeenCalled();
+    expect($formMock.password.$setValidity).toHaveBeenCalled();
+  });
+
+  it('>> loginController.auth() - should set formController submitted to apply validation errors in UI',function() {
+
+    $loginController.model.username = "wronguser";
+    $loginController.model.password = "wrongpass";
+
+    $loginController.auth($formMock);
+
+    $rootScope.$apply(); // resolve promises in the services
+
+    expect($formMock.$setSubmitted).toHaveBeenCalled();
+  });
+
+  it('>> loginController.auth() - if form success all validations should hide dialog for login-component',function() {
+
+    $loginController.model.username = "myuser";
+    $loginController.model.password = "mypass";
+
+    $loginController.auth($formMock);
+
+    $rootScope.$apply(); // resolve promises in the services
+
+    expect(loginDialogSpy.hide).toHaveBeenCalled();
+  });
+
+  it('>> loginController.auth() - if form success all validations should redirect with $location provider',function() {
+
+    $loginController.model.username = "myuser";
+    $loginController.model.password = "mypass";
+
+    $loginController.auth($formMock);
+
+    $rootScope.$apply(); // resolve promises in the services
+
+    expect(locationSpy.url).toHaveBeenCalled();
   });
 
 });
