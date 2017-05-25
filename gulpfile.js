140b72e70df01a7df9279be044c38991290ebfd6
Javier Girón <javierlog08@gmail.com>
2017-05-24 23:09:48 -0500

    Added doc system to the project, you can now generate docs using gulp docs command

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var shell = require('gulp-shell');
 
 gulp.task('sass', function () {
   return gulp.src('./app/**/*.scss')
@@ -15,8 +16,18 @@ gulp.task('sass:watch', function () {
   gulp.watch('./app/**/*.scss', ['sass']);
 });
 
+gulp.task('docs', shell.task([
+  'node '+
+  'node_modules/jsdoc/jsdoc.js ' +
+  '-c jsdoc.conf.json ' +   // config file
+  '-t node_modules/angular-jsdoc/default ' +   // template file
+  '-d docs ' +      // output directory
+  './README.md ' +  // to include README.md as index contents
+  '-r '
+  //'-r app'
+]));
 
-gulp.task('default', function() {
-  
+gulp.task('default', function () {
+
 });
 
